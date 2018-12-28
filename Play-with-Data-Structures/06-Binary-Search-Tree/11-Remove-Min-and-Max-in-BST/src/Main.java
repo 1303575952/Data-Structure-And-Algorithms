a056043f96855e13ae394aead7477fcf4859fa65
1303575952@qq.com <1303575952@qq.com>
2018-12-28 22:19:06 +0800

    11-Remove-Min-and-Max-in-BST

--- Play-with-Data-Structures/06-Binary-Search-Tree/11-Remove-Min-and-Max-in-BST/src/Main.java
+++ Play-with-Data-Structures/06-Binary-Search-Tree/11-Remove-Min-and-Max-in-BST/src/Main.java
@@ -11,31 +11,31 @@ public class Main {
         int n = 1000;
 
         // test removeMin
-        for(int i = 0 ; i < n ; i ++)
+        for (int i = 0; i < n; i++)
             bst.add(random.nextInt(10000));
 
         ArrayList<Integer> nums = new ArrayList<>();
-        while(!bst.isEmpty())
+        while (!bst.isEmpty())
             nums.add(bst.removeMin());
 
         System.out.println(nums);
-        for(int i = 1 ; i < nums.size() ; i ++)
-            if(nums.get(i - 1) > nums.get(i))
+        for (int i = 1; i < nums.size(); i++)
+            if (nums.get(i - 1) > nums.get(i))
                 throw new IllegalArgumentException("Error!");
         System.out.println("removeMin test completed.");
 
 
         // test removeMax
-        for(int i = 0 ; i < n ; i ++)
+        for (int i = 0; i < n; i++)
             bst.add(random.nextInt(10000));
 
         nums = new ArrayList<>();
-        while(!bst.isEmpty())
+        while (!bst.isEmpty())
             nums.add(bst.removeMax());
 
         System.out.println(nums);
-        for(int i = 1 ; i < nums.size() ; i ++)
-            if(nums.get(i - 1) < nums.get(i))
+        for (int i = 1; i < nums.size(); i++)
+            if (nums.get(i - 1) < nums.get(i))
                 throw new IllegalArgumentException("Error!");
         System.out.println("removeMax test completed.");
     }
