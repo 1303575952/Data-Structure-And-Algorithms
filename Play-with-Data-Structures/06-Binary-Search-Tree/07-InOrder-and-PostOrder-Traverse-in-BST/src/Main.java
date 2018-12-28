a77a29e27d3b4b24a93e400651fd7133638065f7
1303575952@qq.com <1303575952@qq.com>
2018-12-28 11:41:33 +0800

    07-InOrder-and-PostOrder-Traverse-in-BST

--- Play-with-Data-Structures/06-Binary-Search-Tree/07-InOrder-and-PostOrder-Traverse-in-BST/src/Main.java
+++ Play-with-Data-Structures/06-Binary-Search-Tree/07-InOrder-and-PostOrder-Traverse-in-BST/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         BST<Integer> bst = new BST<>();
         int[] nums = {5, 3, 6, 8, 4, 2};
-        for(int num: nums)
+        for (int num : nums)
             bst.add(num);
 
         /////////////////
