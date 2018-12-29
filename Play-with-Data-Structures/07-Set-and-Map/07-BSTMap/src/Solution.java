079cbddbab6d586369d5ded10d3f2cf835d048de
1303575952@qq.com <1303575952@qq.com>
2018-12-29 13:59:35 +0800

    07-BSTMap

--- Play-with-Data-Structures/07-Set-and-Map/07-BSTMap/src/Solution.java
+++ Play-with-Data-Structures/07-Set-and-Map/07-BSTMap/src/Solution.java
@@ -11,22 +11,28 @@ public class Solution {
     private interface Map<K, V> {
 
         void add(K key, V value);
+
         boolean contains(K key);
+
         V get(K key);
+
         void set(K key, V newValue);
+
         V remove(K key);
+
         int getSize();
+
         boolean isEmpty();
     }
 
     private class BSTMap<K extends Comparable<K>, V> implements Map<K, V> {
 
-        private class Node{
+        private class Node {
             public K key;
             public V value;
             public Node left, right;
 
-            public Node(K key, V value){
+            public Node(K key, V value) {
                 this.key = key;
                 this.value = value;
                 left = null;
@@ -37,39 +43,39 @@ public class Solution {
         private Node root;
         private int size;
 
-        public BSTMap(){
+        public BSTMap() {
             root = null;
             size = 0;
         }
 
         @Override
-        public int getSize(){
+        public int getSize() {
             return size;
         }
 
         @Override
-        public boolean isEmpty(){
+        public boolean isEmpty() {
             return size == 0;
         }
 
         // 向二分搜索树中添加新的元素(key, value)
         @Override
-        public void add(K key, V value){
+        public void add(K key, V value) {
             root = add(root, key, value);
         }
 
         // 向以node为根的二分搜索树中插入元素(key, value)，递归算法
         // 返回插入新节点后二分搜索树的根
-        private Node add(Node node, K key, V value){
+        private Node add(Node node, K key, V value) {
 
-            if(node == null){
-                size ++;
+            if (node == null) {
+                size++;
                 return new Node(key, value);
             }
 
-            if(key.compareTo(node.key) < 0)
+            if (key.compareTo(node.key) < 0)
                 node.left = add(node.left, key, value);
-            else if(key.compareTo(node.key) > 0)
+            else if (key.compareTo(node.key) > 0)
                 node.right = add(node.right, key, value);
             else // key.compareTo(node.key) == 0
                 node.value = value;
@@ -78,55 +84,55 @@ public class Solution {
         }
 
         // 返回以node为根节点的二分搜索树中，key所在的节点
-        private Node getNode(Node node, K key){
+        private Node getNode(Node node, K key) {
 
-            if(node == null)
+            if (node == null)
                 return null;
 
-            if(key.equals(node.key))
+            if (key.equals(node.key))
                 return node;
-            else if(key.compareTo(node.key) < 0)
+            else if (key.compareTo(node.key) < 0)
                 return getNode(node.left, key);
             else // if(key.compareTo(node.key) > 0)
                 return getNode(node.right, key);
         }
 
         @Override
-        public boolean contains(K key){
+        public boolean contains(K key) {
             return getNode(root, key) != null;
         }
 
         @Override
-        public V get(K key){
+        public V get(K key) {
 
             Node node = getNode(root, key);
             return node == null ? null : node.value;
         }
 
         @Override
-        public void set(K key, V newValue){
+        public void set(K key, V newValue) {
             Node node = getNode(root, key);
-            if(node == null)
+            if (node == null)
                 throw new IllegalArgumentException(key + " doesn't exist!");
 
             node.value = newValue;
         }
 
         // 返回以node为根的二分搜索树的最小值所在的节点
-        private Node minimum(Node node){
-            if(node.left == null)
+        private Node minimum(Node node) {
+            if (node.left == null)
                 return node;
             return minimum(node.left);
         }
 
         // 删除掉以node为根的二分搜索树中的最小节点
         // 返回删除节点后新的二分搜索树的根
-        private Node removeMin(Node node){
+        private Node removeMin(Node node) {
 
-            if(node.left == null){
+            if (node.left == null) {
                 Node rightNode = node.right;
                 node.right = null;
-                size --;
+                size--;
                 return rightNode;
             }
 
@@ -136,44 +142,42 @@ public class Solution {
 
         // 从二分搜索树中删除键为key的节点
         @Override
-        public V remove(K key){
+        public V remove(K key) {
 
             Node node = getNode(root, key);
-            if(node != null){
+            if (node != null) {
                 root = remove(root, key);
                 return node.value;
             }
             return null;
         }
 
-        private Node remove(Node node, K key){
+        private Node remove(Node node, K key) {
 
-            if( node == null )
+            if (node == null)
                 return null;
 
-            if( key.compareTo(node.key) < 0 ){
-                node.left = remove(node.left , key);
+            if (key.compareTo(node.key) < 0) {
+                node.left = remove(node.left, key);
                 return node;
-            }
-            else if(key.compareTo(node.key) > 0 ){
+            } else if (key.compareTo(node.key) > 0) {
                 node.right = remove(node.right, key);
                 return node;
-            }
-            else{   // key.compareTo(node.key) == 0
+            } else {   // key.compareTo(node.key) == 0
 
                 // 待删除节点左子树为空的情况
-                if(node.left == null){
+                if (node.left == null) {
                     Node rightNode = node.right;
                     node.right = null;
-                    size --;
+                    size--;
                     return rightNode;
                 }
 
                 // 待删除节点右子树为空的情况
-                if(node.right == null){
+                if (node.right == null) {
                     Node leftNode = node.left;
                     node.left = null;
-                    size --;
+                    size--;
                     return leftNode;
                 }
 
@@ -195,25 +199,25 @@ public class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
 
         BSTMap<Integer, Integer> map = new BSTMap<>();
-        for(int num: nums1){
-            if(!map.contains(num))
+        for (int num : nums1) {
+            if (!map.contains(num))
                 map.add(num, 1);
             else
                 map.set(num, map.get(num) + 1);
         }
 
         ArrayList<Integer> res = new ArrayList<>();
-        for(int num: nums2){
-            if(map.contains(num)){
+        for (int num : nums2) {
+            if (map.contains(num)) {
                 res.add(num);
                 map.set(num, map.get(num) - 1);
-                if(map.get(num) == 0)
+                if (map.get(num) == 0)
                     map.remove(num);
             }
         }
 
         int[] ret = new int[res.size()];
-        for(int i = 0 ; i < res.size() ; i ++)
+        for (int i = 0; i < res.size(); i++)
             ret[i] = res.get(i);
 
         return ret;
