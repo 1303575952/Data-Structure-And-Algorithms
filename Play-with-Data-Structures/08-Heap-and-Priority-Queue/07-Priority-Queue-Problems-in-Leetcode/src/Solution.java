de3d9676f5874ec7e167b3a2eba46ac7b869a952
1303575952@qq.com <1303575952@qq.com>
2018-12-31 10:41:06 +0800

    07-Priority-Queue-Problems-in-Leetcode

--- Play-with-Data-Structures/08-Heap-and-Priority-Queue/07-Priority-Queue-Problems-in-Leetcode/src/Solution.java
+++ Play-with-Data-Structures/08-Heap-and-Priority-Queue/07-Priority-Queue-Problems-in-Leetcode/src/Solution.java
@@ -13,133 +13,133 @@ class Solution {
         private int size;
 
         // 构造函数，传入数组的容量capacity构造Array
-        public Array(int capacity){
-            data = (E[])new Object[capacity];
+        public Array(int capacity) {
+            data = (E[]) new Object[capacity];
             size = 0;
         }
 
         // 无参数的构造函数，默认数组的容量capacity=10
-        public Array(){
+        public Array() {
             this(10);
         }
 
-        public Array(E[] arr){
-            data = (E[])new Object[arr.length];
-            for(int i = 0 ; i < arr.length ; i ++)
+        public Array(E[] arr) {
+            data = (E[]) new Object[arr.length];
+            for (int i = 0; i < arr.length; i++)
                 data[i] = arr[i];
             size = arr.length;
         }
 
         // 获取数组的容量
-        public int getCapacity(){
+        public int getCapacity() {
             return data.length;
         }
 
         // 获取数组中的元素个数
-        public int getSize(){
+        public int getSize() {
             return size;
         }
 
         // 返回数组是否为空
-        public boolean isEmpty(){
+        public boolean isEmpty() {
             return size == 0;
         }
 
         // 在index索引的位置插入一个新元素e
-        public void add(int index, E e){
+        public void add(int index, E e) {
 
-            if(index < 0 || index > size)
+            if (index < 0 || index > size)
                 throw new IllegalArgumentException("Add failed. Require index >= 0 and index <= size.");
 
-            if(size == data.length)
+            if (size == data.length)
                 resize(2 * data.length);
 
-            for(int i = size - 1; i >= index ; i --)
+            for (int i = size - 1; i >= index; i--)
                 data[i + 1] = data[i];
 
             data[index] = e;
 
-            size ++;
+            size++;
         }
 
         // 向所有元素后添加一个新元素
-        public void addLast(E e){
+        public void addLast(E e) {
             add(size, e);
         }
 
         // 在所有元素前添加一个新元素
-        public void addFirst(E e){
+        public void addFirst(E e) {
             add(0, e);
         }
 
         // 获取index索引位置的元素
-        public E get(int index){
-            if(index < 0 || index >= size)
+        public E get(int index) {
+            if (index < 0 || index >= size)
                 throw new IllegalArgumentException("Get failed. Index is illegal.");
             return data[index];
         }
 
         // 修改index索引位置的元素为e
-        public void set(int index, E e){
-            if(index < 0 || index >= size)
+        public void set(int index, E e) {
+            if (index < 0 || index >= size)
                 throw new IllegalArgumentException("Set failed. Index is illegal.");
             data[index] = e;
         }
 
         // 查找数组中是否有元素e
-        public boolean contains(E e){
-            for(int i = 0 ; i < size ; i ++){
-                if(data[i].equals(e))
+        public boolean contains(E e) {
+            for (int i = 0; i < size; i++) {
+                if (data[i].equals(e))
                     return true;
             }
             return false;
         }
 
         // 查找数组中元素e所在的索引，如果不存在元素e，则返回-1
-        public int find(E e){
-            for(int i = 0 ; i < size ; i ++){
-                if(data[i].equals(e))
+        public int find(E e) {
+            for (int i = 0; i < size; i++) {
+                if (data[i].equals(e))
                     return i;
             }
             return -1;
         }
 
         // 从数组中删除index位置的元素, 返回删除的元素
-        public E remove(int index){
-            if(index < 0 || index >= size)
+        public E remove(int index) {
+            if (index < 0 || index >= size)
                 throw new IllegalArgumentException("Remove failed. Index is illegal.");
 
             E ret = data[index];
-            for(int i = index + 1 ; i < size ; i ++)
+            for (int i = index + 1; i < size; i++)
                 data[i - 1] = data[i];
-            size --;
+            size--;
             data[size] = null; // loitering objects != memory leak
 
-            if(size == data.length / 4 && data.length / 2 != 0)
+            if (size == data.length / 4 && data.length / 2 != 0)
                 resize(data.length / 2);
             return ret;
         }
 
         // 从数组中删除第一个元素, 返回删除的元素
-        public E removeFirst(){
+        public E removeFirst() {
             return remove(0);
         }
 
         // 从数组中删除最后一个元素, 返回删除的元素
-        public E removeLast(){
+        public E removeLast() {
             return remove(size - 1);
         }
 
         // 从数组中删除元素e
-        public void removeElement(E e){
+        public void removeElement(E e) {
             int index = find(e);
-            if(index != -1)
+            if (index != -1)
                 remove(index);
         }
 
-        public void swap(int i, int j){
+        public void swap(int i, int j) {
 
-            if(i < 0 || i >= size || j < 0 || j >= size)
+            if (i < 0 || i >= size || j < 0 || j >= size)
                 throw new IllegalArgumentException("Index is illegal.");
 
             E t = data[i];
@@ -148,14 +148,14 @@ class Solution {
         }
 
         @Override
-        public String toString(){
+        public String toString() {
 
             StringBuilder res = new StringBuilder();
             res.append(String.format("Array: size = %d , capacity = %d\n", size, data.length));
             res.append('[');
-            for(int i = 0 ; i < size ; i ++){
+            for (int i = 0; i < size; i++) {
                 res.append(data[i]);
-                if(i != size - 1)
+                if (i != size - 1)
                     res.append(", ");
             }
             res.append(']');
@@ -163,10 +163,10 @@ class Solution {
         }
 
         // 将数组空间的容量变成newCapacity大小
-        private void resize(int newCapacity){
+        private void resize(int newCapacity) {
 
-            E[] newData = (E[])new Object[newCapacity];
-            for(int i = 0 ; i < size ; i ++)
+            E[] newData = (E[]) new Object[newCapacity];
+            for (int i = 0; i < size; i++)
                 newData[i] = data[i];
             data = newData;
         }
@@ -176,70 +176,70 @@ class Solution {
 
         private Array<E> data;
 
-        public MaxHeap(int capacity){
+        public MaxHeap(int capacity) {
             data = new Array<>(capacity);
         }
 
-        public MaxHeap(){
+        public MaxHeap() {
             data = new Array<>();
         }
 
-        public MaxHeap(E[] arr){
+        public MaxHeap(E[] arr) {
             data = new Array<>(arr);
-            for(int i = parent(arr.length - 1) ; i >= 0 ; i --)
+            for (int i = parent(arr.length - 1); i >= 0; i--)
                 siftDown(i);
         }
 
         // 返回堆中的元素个数
-        public int size(){
+        public int size() {
             return data.getSize();
         }
 
         // 返回一个布尔值, 表示堆中是否为空
-        public boolean isEmpty(){
+        public boolean isEmpty() {
             return data.isEmpty();
         }
 
         // 返回完全二叉树的数组表示中，一个索引所表示的元素的父亲节点的索引
-        private int parent(int index){
-            if(index == 0)
+        private int parent(int index) {
+            if (index == 0)
                 throw new IllegalArgumentException("index-0 doesn't have parent.");
             return (index - 1) / 2;
         }
 
         // 返回完全二叉树的数组表示中，一个索引所表示的元素的左孩子节点的索引
-        private int leftChild(int index){
+        private int leftChild(int index) {
             return index * 2 + 1;
         }
 
         // 返回完全二叉树的数组表示中，一个索引所表示的元素的右孩子节点的索引
-        private int rightChild(int index){
+        private int rightChild(int index) {
             return index * 2 + 2;
         }
 
         // 向堆中添加元素
-        public void add(E e){
+        public void add(E e) {
             data.addLast(e);
             siftUp(data.getSize() - 1);
         }
 
-        private void siftUp(int k){
+        private void siftUp(int k) {
 
-            while(k > 0 && data.get(parent(k)).compareTo(data.get(k)) < 0 ){
+            while (k > 0 && data.get(parent(k)).compareTo(data.get(k)) < 0) {
                 data.swap(k, parent(k));
                 k = parent(k);
             }
         }
 
         // 看堆中的最大元素
-        public E findMax(){
-            if(data.getSize() == 0)
+        public E findMax() {
+            if (data.getSize() == 0)
                 throw new IllegalArgumentException("Can not findMax when heap is empty.");
             return data.get(0);
         }
 
         // 取出堆中最大元素
-        public E extractMax(){
+        public E extractMax() {
 
             E ret = findMax();
 
@@ -250,16 +250,16 @@ class Solution {
             return ret;
         }
 
-        private void siftDown(int k){
+        private void siftDown(int k) {
 
-            while(leftChild(k) < data.getSize()){
+            while (leftChild(k) < data.getSize()) {
                 int j = leftChild(k); // 在此轮循环中,data[k]和data[j]交换位置
-                if( j + 1 < data.getSize() &&
-                        data.get(j + 1).compareTo(data.get(j)) > 0 )
-                    j ++;
+                if (j + 1 < data.getSize() &&
+                        data.get(j + 1).compareTo(data.get(j)) > 0)
+                    j++;
                 // data[j] 是 leftChild 和 rightChild 中的最大值
 
-                if(data.get(k).compareTo(data.get(j)) >= 0 )
+                if (data.get(k).compareTo(data.get(j)) >= 0)
                     break;
 
                 data.swap(k, j);
@@ -268,7 +268,7 @@ class Solution {
         }
 
         // 取出堆中的最大元素，并且替换成元素e
-        public E replace(E e){
+        public E replace(E e) {
 
             E ret = findMax();
             data.set(0, e);
@@ -280,9 +280,13 @@ class Solution {
     private interface Queue<E> {
 
         int getSize();
+
         boolean isEmpty();
+
         void enqueue(E e);
+
         E dequeue();
+
         E getFront();
     }
 
@@ -290,50 +294,50 @@ class Solution {
 
         private MaxHeap<E> maxHeap;
 
-        public PriorityQueue(){
+        public PriorityQueue() {
             maxHeap = new MaxHeap<>();
         }
 
         @Override
-        public int getSize(){
+        public int getSize() {
             return maxHeap.size();
         }
 
         @Override
-        public boolean isEmpty(){
+        public boolean isEmpty() {
             return maxHeap.isEmpty();
         }
 
         @Override
-        public E getFront(){
+        public E getFront() {
             return maxHeap.findMax();
         }
 
         @Override
-        public void enqueue(E e){
+        public void enqueue(E e) {
             maxHeap.add(e);
         }
 
         @Override
-        public E dequeue(){
+        public E dequeue() {
             return maxHeap.extractMax();
         }
     }
 
-    private class Freq implements Comparable<Freq>{
+    private class Freq implements Comparable<Freq> {
 
         public int e, freq;
 
-        public Freq(int e, int freq){
+        public Freq(int e, int freq) {
             this.e = e;
             this.freq = freq;
         }
 
         @Override
-        public int compareTo(Freq another){
-            if(this.freq < another.freq)
+        public int compareTo(Freq another) {
+            if (this.freq < another.freq)
                 return 1;
-            else if(this.freq > another.freq)
+            else if (this.freq > another.freq)
                 return -1;
             else
                 return 0;
@@ -343,31 +347,31 @@ class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
 
         TreeMap<Integer, Integer> map = new TreeMap<>();
-        for(int num: nums){
-            if(map.containsKey(num))
+        for (int num : nums) {
+            if (map.containsKey(num))
                 map.put(num, map.get(num) + 1);
             else
                 map.put(num, 1);
         }
 
         PriorityQueue<Freq> pq = new PriorityQueue<>();
-        for(int key: map.keySet()){
-            if(pq.getSize() < k)
+        for (int key : map.keySet()) {
+            if (pq.getSize() < k)
                 pq.enqueue(new Freq(key, map.get(key)));
-            else if(map.get(key) > pq.getFront().freq){
+            else if (map.get(key) > pq.getFront().freq) {
                 pq.dequeue();
                 pq.enqueue(new Freq(key, map.get(key)));
             }
         }
 
         LinkedList<Integer> res = new LinkedList<>();
-        while(!pq.isEmpty())
+        while (!pq.isEmpty())
             res.add(pq.dequeue().e);
         return res;
     }
 
-    private static void printList(List<Integer> nums){
-        for(Integer num: nums)
+    private static void printList(List<Integer> nums) {
+        for (Integer num : nums)
             System.out.print(num + " ");
         System.out.println();
     }
