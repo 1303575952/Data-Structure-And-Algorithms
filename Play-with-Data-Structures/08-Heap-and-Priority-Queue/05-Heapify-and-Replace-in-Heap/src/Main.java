6f736edd44add92f3410b2b095b69211ce52d3fd
1303575952@qq.com <1303575952@qq.com>
2018-12-31 09:51:38 +0800

    05-Heapify-and-Replace-in-Heap

--- Play-with-Data-Structures/08-Heap-and-Priority-Queue/05-Heapify-and-Replace-in-Heap/src/Main.java
+++ Play-with-Data-Structures/08-Heap-and-Priority-Queue/05-Heapify-and-Replace-in-Heap/src/Main.java
@@ -2,25 +2,25 @@ import java.util.Random;
 
 public class Main {
 
-    private static double testHeap(Integer[] testData, boolean isHeapify){
+    private static double testHeap(Integer[] testData, boolean isHeapify) {
 
         long startTime = System.nanoTime();
 
         MaxHeap<Integer> maxHeap;
-        if(isHeapify)
+        if (isHeapify)
             maxHeap = new MaxHeap<>(testData);
-        else{
+        else {
             maxHeap = new MaxHeap<>();
-            for(int num: testData)
+            for (int num : testData)
                 maxHeap.add(num);
         }
 
         int[] arr = new int[testData.length];
-        for(int i = 0 ; i < testData.length ; i ++)
+        for (int i = 0; i < testData.length; i++)
             arr[i] = maxHeap.extractMax();
 
-        for(int i = 1 ; i < testData.length ; i ++)
-            if(arr[i-1] < arr[i])
+        for (int i = 1; i < testData.length; i++)
+            if (arr[i - 1] < arr[i])
                 throw new IllegalArgumentException("Error");
         System.out.println("Test MaxHeap completed.");
 
@@ -35,7 +35,7 @@ public class Main {
 
         Random random = new Random();
         Integer[] testData = new Integer[n];
-        for(int i = 0 ; i < n ; i ++)
+        for (int i = 0; i < n; i++)
             testData[i] = random.nextInt(Integer.MAX_VALUE);
 
         double time1 = testHeap(testData, false);
