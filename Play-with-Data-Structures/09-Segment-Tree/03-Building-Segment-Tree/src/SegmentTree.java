5f0ad53da7b92cb1eb6e3e72053aada34fcfbbf3
1303575952@qq.com <1303575952@qq.com>
2019-01-01 13:51:10 +0800

    03-Building-Segment-Tree

--- Play-with-Data-Structures/09-Segment-Tree/03-Building-Segment-Tree/src/SegmentTree.java
+++ Play-with-Data-Structures/09-Segment-Tree/03-Building-Segment-Tree/src/SegmentTree.java
@@ -4,22 +4,22 @@ public class SegmentTree<E> {
     private E[] data;
     private Merger<E> merger;
 
-    public SegmentTree(E[] arr, Merger<E> merger){
+    public SegmentTree(E[] arr, Merger<E> merger) {
 
         this.merger = merger;
 
-        data = (E[])new Object[arr.length];
-        for(int i = 0 ; i < arr.length ; i ++)
+        data = (E[]) new Object[arr.length];
+        for (int i = 0; i < arr.length; i++)
             data[i] = arr[i];
 
-        tree = (E[])new Object[4 * arr.length];
+        tree = (E[]) new Object[4 * arr.length];
         buildSegmentTree(0, 0, arr.length - 1);
     }
 
     // 在treeIndex的位置创建表示区间[l...r]的线段树
-    private void buildSegmentTree(int treeIndex, int l, int r){
+    private void buildSegmentTree(int treeIndex, int l, int r) {
 
-        if(l == r){
+        if (l == r) {
             tree[treeIndex] = data[l];
             return;
         }
@@ -35,37 +35,37 @@ public class SegmentTree<E> {
         tree[treeIndex] = merger.merge(tree[leftTreeIndex], tree[rightTreeIndex]);
     }
 
-    public int getSize(){
+    public int getSize() {
         return data.length;
     }
 
-    public E get(int index){
-        if(index < 0 || index >= data.length)
+    public E get(int index) {
+        if (index < 0 || index >= data.length)
             throw new IllegalArgumentException("Index is illegal.");
         return data[index];
     }
 
     // 返回完全二叉树的数组表示中，一个索引所表示的元素的左孩子节点的索引
-    private int leftChild(int index){
-        return 2*index + 1;
+    private int leftChild(int index) {
+        return 2 * index + 1;
     }
 
     // 返回完全二叉树的数组表示中，一个索引所表示的元素的右孩子节点的索引
-    private int rightChild(int index){
-        return 2*index + 2;
+    private int rightChild(int index) {
+        return 2 * index + 2;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         StringBuilder res = new StringBuilder();
         res.append('[');
-        for(int i = 0 ; i < tree.length ; i ++){
-            if(tree[i] != null)
+        for (int i = 0; i < tree.length; i++) {
+            if (tree[i] != null)
                 res.append(tree[i]);
             else
                 res.append("null");
 
-            if(i != tree.length - 1)
+            if (i != tree.length - 1)
                 res.append(", ");
         }
         res.append(']');
