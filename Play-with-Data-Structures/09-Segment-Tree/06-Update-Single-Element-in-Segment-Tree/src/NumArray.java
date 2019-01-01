95580c9929ee83ed5bae54256040073a2bf73113
1303575952@qq.com <1303575952@qq.com>
2019-01-01 14:13:22 +0800

    06-Update-Single-Element-in-Segment-Tree

--- Play-with-Data-Structures/09-Segment-Tree/06-Update-Single-Element-in-Segment-Tree/src/NumArray.java
+++ Play-with-Data-Structures/09-Segment-Tree/06-Update-Single-Element-in-Segment-Tree/src/NumArray.java
@@ -3,24 +3,25 @@
 class NumArray {
 
     private SegmentTree<Integer> segTree;
+
     public NumArray(int[] nums) {
 
-        if(nums.length != 0){
+        if (nums.length != 0) {
             Integer[] data = new Integer[nums.length];
-            for(int i = 0 ; i < nums.length ; i ++)
+            for (int i = 0; i < nums.length; i++)
                 data[i] = nums[i];
             segTree = new SegmentTree<>(data, (a, b) -> a + b);
         }
     }
 
     public void update(int i, int val) {
-        if(segTree == null)
+        if (segTree == null)
             throw new IllegalArgumentException("Error");
         segTree.set(i, val);
     }
 
     public int sumRange(int i, int j) {
-        if(segTree == null)
+        if (segTree == null)
             throw new IllegalArgumentException("Error");
         return segTree.query(i, j);
     }
