ddc3c4d0647b53f21810ee4ca3b91190068d935e
1303575952@qq.com <1303575952@qq.com>
2019-01-06 12:41:51 +0800

    06-Trie-and-Map

--- Play-with-Data-Structures/10-Trie/06-Trie-and-Map/src/MapSum.java
+++ Play-with-Data-Structures/10-Trie/06-Trie-and-Map/src/MapSum.java
@@ -2,24 +2,26 @@ import java.util.TreeMap;
 
 public class MapSum {
 
-    private class Node{
+    private class Node {
 
         public int value;
         public TreeMap<Character, Node> next;
 
-        public Node(int value){
+        public Node(int value) {
             this.value = value;
             next = new TreeMap<>();
         }
 
-        public Node(){
+        public Node() {
             this(0);
         }
     }
 
     private Node root;
 
-    /** Initialize your data structure here. */
+    /**
+     * Initialize your data structure here.
+     */
     public MapSum() {
 
         root = new Node();
@@ -28,9 +30,9 @@ public class MapSum {
     public void insert(String key, int val) {
 
         Node cur = root;
-        for(int i = 0 ; i < key.length() ; i ++){
+        for (int i = 0; i < key.length(); i++) {
             char c = key.charAt(i);
-            if(cur.next.get(c) == null)
+            if (cur.next.get(c) == null)
                 cur.next.put(c, new Node());
             cur = cur.next.get(c);
         }
@@ -40,9 +42,9 @@ public class MapSum {
     public int sum(String prefix) {
 
         Node cur = root;
-        for(int i = 0 ; i < prefix.length() ; i ++){
+        for (int i = 0; i < prefix.length(); i++) {
             char c = prefix.charAt(i);
-            if(cur.next.get(c) == null)
+            if (cur.next.get(c) == null)
                 return 0;
             cur = cur.next.get(c);
         }
@@ -50,10 +52,10 @@ public class MapSum {
         return sum(cur);
     }
 
-    private int sum(Node node){
+    private int sum(Node node) {
 
         int res = node.value;
-        for(char c: node.next.keySet())
+        for (char c : node.next.keySet())
             res += sum(node.next.get(c));
         return res;
     }
