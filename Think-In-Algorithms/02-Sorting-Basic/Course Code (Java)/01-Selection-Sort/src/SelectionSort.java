a8b36ac947e6c98eb05962236bf085b70e697e45
1303575952@qq.com <1303575952@qq.com>
2019-01-11 00:49:47 +0800

    01-Selection-Sort

---
+++ Think-In-Algorithms/02-Sorting-Basic/Course Code (Java)/01-Selection-Sort/src/SelectionSort.java
@@ -0,0 +1,36 @@
+public class SelectionSort {
+    //本算法类不允许产生任何实例
+    private SelectionSort() {
+
+    }
+
+    public static void sort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            // 寻找[i,n)中最小值的索引
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            swap(arr, i, minIndex);
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        SelectionSort.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+}
