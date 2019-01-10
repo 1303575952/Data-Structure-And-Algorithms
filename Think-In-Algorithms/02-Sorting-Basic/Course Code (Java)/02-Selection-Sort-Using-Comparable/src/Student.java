9574c68e8f33766d0c19b79b706ad9ce0c14cc3e
1303575952@qq.com <1303575952@qq.com>
2019-01-11 01:14:46 +0800

    02-Selection-Sort-Using-Comparable

--- Think-In-Algorithms/02-Sorting-Basic/Course Code (Java)/02-Selection-Sort-Using-Comparable/src/Student.java
+++ Think-In-Algorithms/02-Sorting-Basic/Course Code (Java)/02-Selection-Sort-Using-Comparable/src/Student.java
@@ -1,13 +1,9 @@
-package bobo.algo;
-
-import java.util.*;
-
 public class Student implements Comparable<Student> {
 
     private String name;
     private int score;
 
-    public Student(String name, int score){
+    public Student(String name, int score) {
         this.name = name;
         this.score = score;
     }
@@ -18,10 +14,10 @@ public class Student implements Comparable<Student> {
     @Override
     public int compareTo(Student that) {
 
-        if( this.score < that.score )
-            return -1;
-        else if( this.score > that.score )
+        if (this.score < that.score)
             return 1;
+        else if (this.score > that.score)
+            return -1;
         else // this.score == that.score
             return this.name.compareTo(that.name);
     }
@@ -29,6 +25,6 @@ public class Student implements Comparable<Student> {
     // 定义Student实例的打印输出方式
     @Override
     public String toString() {
-        return "Student: " + this.name + " " + Integer.toString( this.score );
+        return "Student: " + this.name + " " + Integer.toString(this.score);
     }
 }
