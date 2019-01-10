856116d26f30e2ab153ab02a5acc9234fe3daba0
1303575952@qq.com <1303575952@qq.com>
2019-01-10 21:42:05 +0800

    05-Hash-Table-Implementation

--- Play-with-Data-Structures/14-Hash-Table/05-Hash-Table-Implementation/src/HashTable.java
+++ Play-with-Data-Structures/14-Hash-Table/05-Hash-Table-Implementation/src/HashTable.java
@@ -6,59 +6,59 @@ public class HashTable<K, V> {
     private int size;
     private int M;
 
-    public HashTable(int M){
+    public HashTable(int M) {
         this.M = M;
         size = 0;
         hashtable = new TreeMap[M];
-        for(int i = 0 ; i < M ; i ++)
+        for (int i = 0; i < M; i++)
             hashtable[i] = new TreeMap<>();
     }
 
-    public HashTable(){
+    public HashTable() {
         this(97);
     }
 
-    private int hash(K key){
+    private int hash(K key) {
         return (key.hashCode() & 0x7fffffff) % M;
     }
 
-    public int getSize(){
+    public int getSize() {
         return size;
     }
 
-    public void add(K key, V value){
+    public void add(K key, V value) {
         TreeMap<K, V> map = hashtable[hash(key)];
-        if(map.containsKey(key))
+        if (map.containsKey(key))
             map.put(key, value);
-        else{
+        else {
             map.put(key, value);
-            size ++;
+            size++;
         }
     }
 
-    public V remove(K key){
+    public V remove(K key) {
         V ret = null;
         TreeMap<K, V> map = hashtable[hash(key)];
-        if(map.containsKey(key)){
+        if (map.containsKey(key)) {
             ret = map.remove(key);
-            size --;
+            size--;
         }
         return ret;
     }
 
-    public void set(K key, V value){
+    public void set(K key, V value) {
         TreeMap<K, V> map = hashtable[hash(key)];
-        if(!map.containsKey(key))
+        if (!map.containsKey(key))
             throw new IllegalArgumentException(key + " doesn't exist!");
 
         map.put(key, value);
     }
 
-    public boolean contains(K key){
+    public boolean contains(K key) {
         return hashtable[hash(key)].containsKey(key);
     }
 
-    public V get(K key){
+    public V get(K key) {
         return hashtable[hash(key)].get(key);
     }
 }
