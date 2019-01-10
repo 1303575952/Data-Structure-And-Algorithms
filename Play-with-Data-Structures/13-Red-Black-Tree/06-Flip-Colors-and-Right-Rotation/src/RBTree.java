1d1516c5130bd7f607b7c1cd1d1a94c88367d993
1303575952@qq.com <1303575952@qq.com>
2019-01-10 19:03:26 +0800

    06-Flip-Colors-and-Right-Rotation

--- Play-with-Data-Structures/13-Red-Black-Tree/06-Flip-Colors-and-Right-Rotation/src/RBTree.java
+++ Play-with-Data-Structures/13-Red-Black-Tree/06-Flip-Colors-and-Right-Rotation/src/RBTree.java
@@ -5,13 +5,13 @@ public class RBTree<K extends Comparable<K>, V> {
     private static final boolean RED = true;
     private static final boolean BLACK = false;
 
-    private class Node{
+    private class Node {
         public K key;
         public V value;
         public Node left, right;
         public boolean color;
 
-        public Node(K key, V value){
+        public Node(K key, V value) {
             this.key = key;
             this.value = value;
             left = null;
@@ -23,22 +23,22 @@ public class RBTree<K extends Comparable<K>, V> {
     private Node root;
     private int size;
 
-    public RBTree(){
+    public RBTree() {
         root = null;
         size = 0;
     }
 
-    public int getSize(){
+    public int getSize() {
         return size;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
     // 判断节点node的颜色
-    private boolean isRed(Node node){
-        if(node == null)
+    private boolean isRed(Node node) {
+        if (node == null)
             return BLACK;
         return node.color;
     }
@@ -48,7 +48,7 @@ public class RBTree<K extends Comparable<K>, V> {
     // T1   x   --------->   node   T3
     //     / \              /   \
     //    T2 T3            T1   T2
-    private Node leftRotate(Node node){
+    private Node leftRotate(Node node) {
 
         Node x = node.right;
 
@@ -67,7 +67,7 @@ public class RBTree<K extends Comparable<K>, V> {
     //   x    T2   ------->   y   node
     //  / \                       /  \
     // y  T1                     T1  T2
-    private Node rightRotate(Node node){
+    private Node rightRotate(Node node) {
 
         Node x = node.left;
 
@@ -82,7 +82,7 @@ public class RBTree<K extends Comparable<K>, V> {
     }
 
     // 颜色翻转
-    private void flipColors(Node node){
+    private void flipColors(Node node) {
 
         node.color = RED;
         node.left.color = BLACK;
@@ -90,23 +90,23 @@ public class RBTree<K extends Comparable<K>, V> {
     }
 
     // 向红黑树中添加新的元素(key, value)
-    public void add(K key, V value){
+    public void add(K key, V value) {
         root = add(root, key, value);
         root.color = BLACK; // 最终根节点为黑色节点
     }
 
     // 向以node为根的红黑树中插入元素(key, value)，递归算法
     // 返回插入新节点后红黑树的根
-    private Node add(Node node, K key, V value){
+    private Node add(Node node, K key, V value) {
 
-        if(node == null){
-            size ++;
+        if (node == null) {
+            size++;
             return new Node(key, value); // 默认插入红色节点
         }
 
-        if(key.compareTo(node.key) < 0)
+        if (key.compareTo(node.key) < 0)
             node.left = add(node.left, key, value);
-        else if(key.compareTo(node.key) > 0)
+        else if (key.compareTo(node.key) > 0)
             node.right = add(node.right, key, value);
         else // key.compareTo(node.key) == 0
             node.value = value;
@@ -115,52 +115,52 @@ public class RBTree<K extends Comparable<K>, V> {
     }
 
     // 返回以node为根节点的二分搜索树中，key所在的节点
-    private Node getNode(Node node, K key){
+    private Node getNode(Node node, K key) {
 
-        if(node == null)
+        if (node == null)
             return null;
 
-        if(key.equals(node.key))
+        if (key.equals(node.key))
             return node;
-        else if(key.compareTo(node.key) < 0)
+        else if (key.compareTo(node.key) < 0)
             return getNode(node.left, key);
         else // if(key.compareTo(node.key) > 0)
             return getNode(node.right, key);
     }
 
-    public boolean contains(K key){
+    public boolean contains(K key) {
         return getNode(root, key) != null;
     }
 
-    public V get(K key){
+    public V get(K key) {
 
         Node node = getNode(root, key);
         return node == null ? null : node.value;
     }
 
-    public void set(K key, V newValue){
+    public void set(K key, V newValue) {
         Node node = getNode(root, key);
-        if(node == null)
+        if (node == null)
             throw new IllegalArgumentException(key + " doesn't exist!");
 
         node.value = newValue;
     }
 
     // 返回以node为根的二分搜索树的最小值所在的节点
-    private Node minimum(Node node){
-        if(node.left == null)
+    private Node minimum(Node node) {
+        if (node.left == null)
             return node;
         return minimum(node.left);
     }
 
     // 删除掉以node为根的二分搜索树中的最小节点
     // 返回删除节点后新的二分搜索树的根
-    private Node removeMin(Node node){
+    private Node removeMin(Node node) {
 
-        if(node.left == null){
+        if (node.left == null) {
             Node rightNode = node.right;
             node.right = null;
-            size --;
+            size--;
             return rightNode;
         }
 
@@ -169,44 +169,42 @@ public class RBTree<K extends Comparable<K>, V> {
     }
 
     // 从二分搜索树中删除键为key的节点
-    public V remove(K key){
+    public V remove(K key) {
 
         Node node = getNode(root, key);
-        if(node != null){
+        if (node != null) {
             root = remove(root, key);
             return node.value;
         }
         return null;
     }
 
-    private Node remove(Node node, K key){
+    private Node remove(Node node, K key) {
 
-        if( node == null )
+        if (node == null)
             return null;
 
-        if( key.compareTo(node.key) < 0 ){
-            node.left = remove(node.left , key);
+        if (key.compareTo(node.key) < 0) {
+            node.left = remove(node.left, key);
             return node;
-        }
-        else if(key.compareTo(node.key) > 0 ){
+        } else if (key.compareTo(node.key) > 0) {
             node.right = remove(node.right, key);
             return node;
-        }
-        else{   // key.compareTo(node.key) == 0
+        } else {   // key.compareTo(node.key) == 0
 
             // 待删除节点左子树为空的情况
-            if(node.left == null){
+            if (node.left == null) {
                 Node rightNode = node.right;
                 node.right = null;
-                size --;
+                size--;
                 return rightNode;
             }
 
             // 待删除节点右子树为空的情况
-            if(node.right == null){
+            if (node.right == null) {
                 Node leftNode = node.left;
                 node.left = null;
-                size --;
+                size--;
                 return leftNode;
             }
 
@@ -224,12 +222,12 @@ public class RBTree<K extends Comparable<K>, V> {
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         System.out.println("Pride and Prejudice");
 
         ArrayList<String> words = new ArrayList<>();
-        if(FileOperation.readFile("pride-and-prejudice.txt", words)) {
+        if (FileOperation.readFile("pride-and-prejudice.txt", words)) {
             System.out.println("Total words: " + words.size());
 
             RBTree<String, Integer> map = new RBTree<>();
