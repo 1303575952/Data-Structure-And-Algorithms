cab95190a452bdc4d2492e39b2bac356da1ce630
1303575952@qq.com <1303575952@qq.com>
2019-01-11 22:16:53 +0800

    03-Selection-Sort-Generate-Test-Cases

---
+++ Think-In-Algorithms/02-Sorting-Basic/Course Code (Java)/03-Selection-Sort-Generate-Test-Cases/src/SortTestHelper.java
@@ -0,0 +1,25 @@
+public class SortTestHelper {
+    // SortTestHelper不允许产生任何实例
+    private SortTestHelper() {
+
+    }
+
+    //生成有n个元素的随机数组，每个元素随机范围[rangeL, rangeR]
+    public static Integer[] generateRandomArray(int n, int rangeL, int rangeR) {
+        assert rangeL < rangeR;
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = new Integer((int) Math.random() * (rangeR - rangeL + 1) + rangeL);
+        }
+
+        return arr;
+    }
+
+    public static void printArray(Object[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+}
